4c98f617528adbf846e7624e5b7fb78706480027
Ashton Sumner <jasumner14@gmail.com>
2021-11-13 09:41:39 -0700

    added SampleGraph array

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
 const Form = () => {
-
     const clickHandler = () => {
         alert('Click');
     }
-
     return (
         <div style={{marginTop:"20px"}}>
             <form>
@@ -16,12 +14,12 @@ const Form = () => {
                 <div className="form-group">
                     <label htmlFor="timewindow">Input Desired Prediction Window </label>
                     <select className="form-control" id="timewindow">
-                        <option>1 Day</option>
-                        <option>3 Days</option>
-                        <option>1 Week</option>
-                        <option>4 Weeks</option>
-                        <option>3 Months</option>
-                        <option>1 Year</option>
+                        <option id="0">1 Day</option>
+                        <option id="1">3 Days</option>
+                        <option id="2">1 Week</option>
+                        <option id="3">4 Weeks</option>
+                        <option id="4">3 Months</option>
+                        <option id="5">1 Year</option>
                     </select>
                 </div>
                 <button onClick={clickHandler} className="btn">Submit</button>
---
+++ src/shared/SAMPLEGRAPHS.js
@@ -0,0 +1,32 @@
+const SAMPLEGRAPH = [
+    {
+        id: 0,
+        imgsrc: '../images/1DaySample.jpeg',
+        imgalt: "Graph 1"
+    },
+    {
+        id: 1,
+        imgsrc: '../images/3DaySample.jpeg',
+        imgalt: "Graph 2"
+    },
+    {
+        id: 2,
+        imgsrc: '../images/1WkSample.jpeg',
+        imgalt: "Graph 3"
+    },
+    {
+        id: 3,
+        imgsrc: '../images/4WkSample.jpeg',
+        imgalt: "Graph 4"
+    },
+    {
+        id: 4,
+        imgsrc: '../images/3MtSample.jpeg',
+        imgalt: "Graph 5"
+    },
+    {
+        id: 5,
+        imgsrc: '../images/1YrSample.jpeg',
+        imgalt: "Graph 6"
+    }
+]
